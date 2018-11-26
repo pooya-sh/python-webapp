423d1b38a47260c8b987f244fcda2ddfd8cc33ff
Pooya <pooya@live.se>
2018-11-26 15:48:53 +0100

    Add Login, Register and Profile

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'PooyaS',
-        'title': 'Blog Post 1',
-        'content':'First post content',
-        'date_posted':'Augost 28, 2018'
-
-    },
-        {
-        'author': 'John D',
-        'title': 'Blog Post 2',
-        'content':'Second post content',
-        'date_posted':'Augost 29, 2018'
-    }
-]
-
+from .models import Post
 
 
 def home(request):
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/home.html', context)
+        context = {
+        'posts': Post.objects.all()
+        }
+        return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
